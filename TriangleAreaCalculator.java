b21faa8203975d320ccb35f51918b6ac5616552f
bhavya <bhavyasree@gmail.com>
2025-10-15 20:51:15 +0530

    scanner examples

---
+++ TriangleAreaCalculator.java
@@ -0,0 +1,19 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class TriangleAreaCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the base of the triangle:");
+		double base = scanner.nextDouble();
+		System.out.println("Enter the Height of the Triangle:");
+		double height = scanner.nextDouble();
+		double area = 0.5 * base * height;
+		System.out.println("Area of Triangle:" + area);
+		scanner.close();
+	}
+
+}
